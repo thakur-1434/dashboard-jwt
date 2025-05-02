42d03fe8b6d77765cd28423ac84ed0d6c7f74436
thakur-1434 <rahulthakurjk321@gmail.com>
2025-05-02 10:27:50 +0530

    Initial commit - Django DRF with JWT auth

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # custom User Manager
 class UserManager(BaseUserManager):
-    def create_user(self, email, name, tc, password=None):
+    def create_user(self, email, name, tc, password=None, password2 = None):
         """
         Creates and saves a User with the given email,name, tc and password.
         """
