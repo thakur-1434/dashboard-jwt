52a12adc8ae3047fd0639e74efe1cbf49d4a421e
thakur-1434 <rahulthakurjk321@gmail.com>
2025-05-22 11:55:21 +0530

    add search and pagination

--- dashboard_project/settings.py
+++ dashboard_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from decouple import config
+# from decouple import config
 from datetime import timedelta
 
 
